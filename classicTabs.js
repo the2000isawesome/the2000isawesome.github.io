d203faf3cb53e43e0be18308ceb495043919011f
the2000isawesome <72530656+the2000isawesome@users.noreply.github.com>
2020-11-28 13:24:38 -0500

    wrong version whoops

--- classicTabs.js
+++ classicTabs.js
@@ -1,8 +1,22 @@
-if (document.getElementById('app') == null) {
-	let nav = document.getElementsByClassName('site-nav')[0];
-	nav.children[2].innerHTML = '<a href=\"/discuss\">Discuss</a>';
-	nav.children[3].innerHTML = '<a href=\"/ideas\">Help</a>';
-} else if (document.getElementById('pagewrapper') == null) {
-	document.getElementsByClassName('ideas')[0].innerHTML = '<a href=\"/discuss\"><span>Discuss</span></a>';
-	document.getElementsByClassName('about')[0].innerHTML = '<a href=\"/ideas\"><span>Help</span></a>';
-}
+// ==UserScript==
+// @name         [Scratch] Classic tab names
+// @namespace    https://the2000isawesome.github.io
+// @version      1.0
+// @description  Restores the "discuss" and "help" tabs
+// @author       the2000
+// @match        *https://scratch.mit.edu/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    if (document.getElementById('app') == null) {
+        let nav = document.getElementsByClassName('site-nav')[0];
+        nav.children[2].innerHTML = '<a href=\"/discuss\">Discuss</a>';
+        nav.children[3].innerHTML = '<a href=\"/ideas\">Help</a>';
+    } else if (document.getElementById('pagewrapper') == null) {
+        document.getElementsByClassName('ideas')[0].innerHTML = '<a href=\"/discuss\"><span>Discuss</span></a>';
+        document.getElementsByClassName('about')[0].innerHTML = '<a href=\"/ideas\"><span>Help</span></a>';
+    }
+})();
