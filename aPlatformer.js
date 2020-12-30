1e761c8048e264c21e78fc5e53b53e8a317377de
the2000isawesome <72530656+the2000isawesome@users.noreply.github.com>
2020-12-29 23:48:49 -0500

    it won't show up on the site ):<

--- aPlatformer.js
+++ aPlatformer.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         [Scratch] Userscript | A platformer
+// @name         [Scratch] Userscript | A Platformer
 // @namespace    https://the2000isawesome.github.io/
 // @version      1.0
-// @description  Adds "A platformer" to the end of all Scratch page titles | A platformer
+// @description  Adds "A platformer" to the end of all Scratch page titles | A Platformer
 // @author       the2000
 // @match        *https://scratch.mit.edu/*
 // @grant        none
