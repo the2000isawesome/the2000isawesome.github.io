b7485b4619d49ef3cb8b14594aa3e26f09cd0516
the2000isawesome <72530656+the2000isawesome@users.noreply.github.com>
2020-12-29 23:59:09 -0500

    might as well stop calling this "very unfinished" since it's mostly finished

--- snapskinuserscript.js
+++ snapskinuserscript.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         [Snap!] the2000's unnamed skin (v0.1)
+// @name         [Snap!] Snatch Skin
 // @namespace    https://the2000isawesome.github.io/
-// @version      0.1
-// @description  A very early version of my Snap! skin. Partially inspired by the Scratch 2.0 design.
+// @version      1.0
+// @description  My Snap! skin. Takes inspiration from the Scratch 2.0 design.
 // @author       the2000
 // @match        *https://snap.berkeley.edu/*
 // @grant        none
