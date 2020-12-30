62f359c30ce94a6843c0ddf3edd22a124c20f5b7
the2000isawesome <72530656+the2000isawesome@users.noreply.github.com>
2020-12-29 23:45:54 -0500

    Create aPlatformer.js

---
+++ aPlatformer.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         [Scratch] Userscript | A platformer
+// @namespace    https://the2000isawesome.github.io/
+// @version      1.0
+// @description  Adds "A platformer" to the end of all Scratch page titles | A platformer
+// @author       the2000
+// @match        *https://scratch.mit.edu/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.title += ' | A Platformer';
+})();
